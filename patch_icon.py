d4d1d96ae72a08dc0227887743e16bd9f772bbd3
chenmld233 <chenmld233@gmail.com>
2026-03-19 12:30:41 +0800

    add HapticFeedback

--- patch_icon.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('lib/main.dart', 'r') as f:
-    content = f.read()
-
-content = re.sub(r'Icon\(\s*Icons\.rss_feed,\s*size:\s*32,', r'Icon(\n                          _getNetworkIcon(),\n                          size: 32,', content)
-
-with open('lib/main.dart', 'w') as f:
-    f.write(content)
--- rewrite_request_page.py
+++
@@ -1,6 +0,0 @@
-import re
-
-with open('lib/pages/request_page.dart', 'r') as f:
-    content = f.read()
-
-# We will just rewrite the whole file, it's easier and cleaner.
